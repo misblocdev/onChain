7a5111590204855b44a6c4d1aea554fec96c95b4
MisBlock <95900291+misblocdev@users.noreply.github.com>
2023-07-13 10:36:22 +0900

    Update App.js

--- takeReact/client/src/App.js
+++ takeReact/client/src/App.js
@@ -1,7 +1,4 @@
-//import logo from './logo.svg'
 import './App.css'
-// import Version from './components/Version'
-// import GetLatestBlock from './components/GetLatestBlock'
 import CreateAccount from './components/CreateAccount'
 import Balance from './components/Balance'
 import AddAccountFromKeyStore from './components/AddAccountFromKeyStore'
@@ -10,29 +7,16 @@ import TransferMemo from './components/TransferMemo'
 import AddMunjin from './components/AddMunjin'
 
 import { crypt } from './components/crypto'
-//import { Component } from 'react'
 
-// 자식에서 부모로 스테이트 변경 시
-//import React, { useState } from 'react'
 
 
 function App() {
-  // const AddMunjinFunction = (x) => {
-  //   console.log(x)
-  // }
-  // return(
-  //   <div className="App">
-  //     <AddMunjin AddMunjinFunction={AddMunjinFunction} />
-  //   </div>
-  // )
-
-  // 오늘 년월일.
   let todayYear = new Date().getFullYear()
   let todayMon = new Date().getMonth()+1
-  let todayMonth = ('00' + todayMon).slice(-2)// 1~9월이면 01~09표기
+  let todayMonth = ('00' + todayMon).slice(-2)
   let todayDate = new Date().getDate()
   let today = `${todayYear}${todayMonth}${todayDate}`
-  // crypto-js 암호화 시켜 props로 TransferMemo.jsx 넘겨줌. 삭제 예정
+
   let enMemo = crypt(`문진 정보를 넣고 암호 버튼을 눌러 주세요-${today}`, true)
 
   console.log()
