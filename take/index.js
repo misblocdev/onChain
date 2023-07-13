1777a70bc400f45b453c46bb7af28eb06bed786e
MisBlock <95900291+misblocdev@users.noreply.github.com>
2023-07-13 10:30:32 +0900

    Update index.js
    주석, 설명 제거

--- take/index.js
+++ take/index.js
@@ -3,13 +3,12 @@ require("dotenv").config()
 const CryptoJS = require('crypto-js')
 const express = require('express')
 const app = express()
-//const Router = require('./router/routes');
 const port = process.env.PORT
 const Caver = require("caver-js")
 const caver = new Caver(process.env.CYPRES2)
-// body-parser 
-app.use(express.json());// json 타입 body 파싱
-app.use(express.urlencoded({ extended: false }));// false:기본 내장 querysting 사용
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // localhost:8882
 app.listen(port,() => {
@@ -17,30 +16,25 @@ app.listen(port,() => {
 })
 // app.post() : get방식으로 http 요청. /=home
 app.post("/", (req, res) => {
-    res.send("start!!")// 요청 성공시 msg
+    res.send("start!!")
 })
 
 app.post('/version',async (req,res) => {
     const version = await caver.rpc.klay.getClientVersion()
     res.send(version)
 })
-/**
- * @param 없음
- * @returns 최근 블럭 번호
- * @param {String} block
- * @returns 블럭 번호에 해당하는 해시
- */
+
 app.post('/getBlock',async (req,res) => {
     const { block = "" } = req?.body
     try {
         if(block) {
             console.log(block)
-            let blk = await caver.klay.getBlock(block)// 해당 블럭 조회
+            let blk = await caver.klay.getBlock(block)
             console.log(blk.hash)
             return res.json(blk.hash)
         }
         else {
-            let block = await caver.klay.getBlockNumber()// 최근 블럭 조회
+            let block = await caver.klay.getBlockNumber()
             return res.json('last block : '+block)
         }
     }
@@ -48,18 +42,12 @@ app.post('/getBlock',async (req,res) => {
         return res.json('블럭 번호 조회 실패')
     }
 })
-/**
- * @param 없음
- * @returns 최근 블럭 번호
- * @param {String} block
-  * @returns 블럭 번호에 해당하는 해시
- */
+
 app.post('/getTx',async (req,res) => {
     const { hash = "" } = req?.body
-    if (hash === null ) throw "인자 없음"
+    if (hash === null ) throw "no Input"
     try {
         const receipt = await caver.klay.getTransactionReceipt(hash)
-        // hex를 utf8로 변경 후 AES256 풀기
         de = await aes256(await caver.utils.hexToUtf8(receipt.input), false)
         return res.json(de)
     }
@@ -68,14 +56,12 @@ app.post('/getTx',async (req,res) => {
     }
 })
 
-
-// 관리자 잔고
 app.post('/getBalance', async (req, res, next) => {
     try {
-        //if (address === "") throw "인자 없음"
-        balance = await caver.klay.getBalance(process.env.ADDRESS2)// 관리자 계정 잔고
+        //if (address === "") throw "No input"
+        balance = await caver.klay.getBalance(process.env.ADDRESS2)
         balance = caver.utils.fromPeb(balance, "KLAY")
-        //const balance = await caver.klay.getBalance(address)// 입력 받은 계정 잔고
+        //const balance = await caver.klay.getBalance(address)
         return res.json('관리자 잔고 : '+balance+'klay')
     }
     catch (error) {
@@ -85,7 +71,7 @@ app.post('/getBalance', async (req, res, next) => {
 
 app.post('/getAccount',async (req,res) => {
     const { address = null } = req?.body
-    if (address === null ) throw "인자 없음"
+    if (address === null ) throw "No input"
     try {
         let getAccount = await caver.klay.getAccount(address)
         console.log(getAccount.account.balance)
@@ -102,11 +88,11 @@ app.post('/transferMemo', async (req,res) => {
     let on = await sendMemo(body)
     res.json(on)
 })
-// AES256 받아 복호
+
 app.post('/decrypt', async (req,res) => {
     const body = req?.body
     try{
-        de = await aes256(body.decrypt, false)// 복호
+        de = await aes256(body.decrypt, false)
         res.json(de)
     }
     catch {
@@ -118,8 +104,8 @@ async function sendMemo(memo) {
     try {
         const aesMemo = await aes256(memo, true)
         const hexMemo = await caver.utils.toHex(aesMemo)
-        const keyring = await caver.wallet.keyring.createFromPrivateKey(process.env.PRIVATE2)// 개인키로 키링 생성
-        await caver.wallet.add(keyring) // caver-js 인메모리 지갑에 keyring 추가
+        const keyring = await caver.wallet.keyring.createFromPrivateKey(process.env.PRIVATE2)
+        await caver.wallet.add(keyring)
         const transactionMemo = await caver.transaction.valueTransferMemo.create({
             type: 'VALUE_TRANSFER_MEMO',
             from: keyring.address,
@@ -129,32 +115,23 @@ async function sendMemo(memo) {
             gas: 750000,
             input: hexMemo
         })
-        const signed = await caver.wallet.sign(keyring.address, transactionMemo)// 서명
-        const receipt = await caver.rpc.klay.sendRawTransaction(signed)// transaction 신청 블럭. 생성 되면 receipt로 영수증 리턴
-        await caver.wallet.remove(keyring.address)// 사용 후 삭제
-        memo = await aes256(await caver.utils.hexToUtf8(receipt.input), false)// 복호
-        //const re = process.env.BAOBAB_PATH + receipt.transactionHash// URL + transaction
+        const signed = await caver.wallet.sign(keyring.address, transactionMemo)
+        const receipt = await caver.rpc.klay.sendRawTransaction(signed)
+        await caver.wallet.remove(keyring.address)
+        memo = await aes256(await caver.utils.hexToUtf8(receipt.input), false)
         const re = receipt.transactionHash
-        return re// 미스블록 작업 해시
+        return re
     }
     catch (error) {
         return res.json('온 체인 작업 실패 : ' + memo)
     }
 }
 
-/**
- * @param {String} val 
- * @param {true, false} option 
- * @returns string 타입과 true를 넣으면 AES256 암호. string 타입과 false를 넣으면 복호.
- */
-// AES256 + AESKEY
 async function aes256(val, option) {
-    // 암호
     if (option === true) {
         var en = CryptoJS.AES.encrypt(JSON.stringify(val), process.env.AESKEY).toString()
         return en
     } else {
-    // 복호
         var bytes = CryptoJS.AES.decrypt(val, process.env.AESKEY)
         var de = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
         return de
